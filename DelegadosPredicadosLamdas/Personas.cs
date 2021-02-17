7aded2c734f92fb30680ddc07fe37e5c5d46bf46
ricardo-alc <ralcantarac568@alumno.uaemex.mx>
2021-02-17 15:13:13 -0600

    Predicados

---
+++ DelegadosPredicadosLamdas/Personas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegadosPredicadosLamdas
+{
+    class Personas
+    {
+
+        private string nombre;
+        private int edad;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Edad { get => edad; set => edad = value; }
+    }
+}
--- DelegadosPredicadosLamdas/Program.cs
+++ DelegadosPredicadosLamdas/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DelegadosPredicadosLamdas
 {
@@ -7,9 +8,15 @@ namespace DelegadosPredicadosLamdas
         static void Main(string[] args)
         {
             ejemploDelegados();
+            ejemploDelegadoPredicado();
+            ejemploPredicados2();
+           
         }
 
-        public static void ejemploDelegados()
+        //Definicion del objeto delegado
+        //delegate tipo nombreDelegado(parametros);
+        delegate void ObjetoDelegado(string msj);
+        static void ejemploDelegados()
         {
             
             //Creacion del objeto delegado apuntando al mensaje bienvenida
@@ -24,10 +31,85 @@ namespace DelegadosPredicadosLamdas
 
         }
 
-        //Definicion del objeto delegado
-        //delegate tipo nombreDelegado(parametros);
-        delegate void ObjetoDelegado(string msj);
+        
 
+        static void ejemploDelegadoPredicado()
+        {
+            List<int> listaNumeros = new List<int>();
+            listaNumeros.AddRange(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+
+            //declaramos delegado predicado
+            Predicate<int> miDelPredicado = new Predicate<int>(damePares);
+
+            List<int> numPares = listaNumeros.FindAll(miDelPredicado);
+
+            foreach (int num in numPares)
+            {
+                Console.WriteLine(num);
+            }
+        }
+
+        static bool damePares(int num)
+        {
+            if (num % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+       
+        
+        static void ejemploPredicados2()
+        {
+            List<Personas> gente = new List<Personas>();
+
+            Personas p1 = new Personas();
+            p1.Nombre = "Josue";
+            p1.Edad = 18;
+
+            Personas p2 = new Personas();
+            p2.Nombre = "Gerardo";
+            p2.Edad = 28;
+
+            Personas p3 = new Personas();
+            p3.Nombre = "Ricardo";
+            p3.Edad = 18;
+
+            gente.AddRange(new Personas[]{ p1,p2,p3});
+
+            Predicate<Personas> predGente = new Predicate<Personas>(existePersona);
+
+            bool existe = gente.Exists(predGente);
+
+            if (existe) Console.WriteLine("Hay personas que se llaman Ricardo");
+            else Console.WriteLine("No hay gente con ese nombre");
+
+            Predicate<Personas> predGenteMayor = new Predicate<Personas>(hayAdultos);
+
+            bool mayores = gente.Exists(predGenteMayor);
+
+            if (mayores) Console.WriteLine("Hay personas Adultas");
+            else Console.WriteLine("No hay mayores de edad");
+
+
+        }
+
+        static bool existePersona(Personas prs)
+        {
+            if (prs.Nombre=="Ricardo") return true;
+            else return false;
+        }
+
+        static bool hayAdultos(Personas prs)
+        {
+            if (prs.Edad >= 18) return true;
+            else return false;
+        }
 
     }
 
